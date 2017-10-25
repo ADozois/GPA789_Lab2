c7e92c2a12283f54ee1c244c605fd157526987cb
guillaumecn <chabot.nobert.guillaume@gmail.com>
2017-10-24 20:55:33 -0400

    First milestone - first tab explorer and viewer

--- XtractCom/XtractCom/XtractCom.cpp
+++ XtractCom/XtractCom/XtractCom.cpp
@@ -1,11 +1,42 @@
 #include "XtractCom.h"
+#include "QFileExplorer.h"
+#include "QCppCommentViewer.h"
+#include <qsplitter.h>
 
 XtractCom::XtractCom(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
-
-	
+	//Set the icon on the main window
 	setWindowIcon(QIcon(":/XtractCom/Icon"));
-	//setCentralWidget();*/
+
+	//Creation de QfileExplorer
+
+	mQFileExplorer = new QFileExplorer;
+	mQCppCommentViewer = new QCppCommentViewer;
+	static QStringList cppLanguageSuffix{ "c", "cpp", "cxx", "h", "hpp", "hxx", "" };
+
+	mQFileExplorer->setFileSuffixFilter(cppLanguageSuffix);
+	mQCppCommentViewer->setFileSuffixFilter(cppLanguageSuffix);
+
+	//Create tabs
+	mTabExplorer = new QTabWidget;
+	mTabExplorer->addTab(mQFileExplorer, "Explorateur de fichier");
+	mTabTask = new QTabWidget;
+	mTabTask->addTab(mQCppCommentViewer, "Consultation");
+	//
+	//mTabTask->addTab(newwidget, "Génération par lot");
+	//mTabTask->addTab(newwidget, "Suppléments");
+
+
+	QSplitter * mainSplitter{ new QSplitter };
+	mainSplitter->addWidget(mTabExplorer);
+	mainSplitter->addWidget(mTabTask);
+	mainSplitter->setCollapsible(0, false);
+	mainSplitter->setCollapsible(1, false);
+
+	setCentralWidget(mainSplitter);
+
+	connect(mQFileExplorer, &QFileExplorer::fileSelected, mQCppCommentViewer, &QCppCommentViewer::setFile);
+	//connect(mQCppCommentViewer, &QCppCommentViewer::eventSignaled, statusBar, &QStatusBar::showMessage);
 }
--- XtractCom/XtractCom/XtractCom.h
+++ XtractCom/XtractCom/XtractCom.h
@@ -3,12 +3,23 @@
 #include <QtWidgets/QMainWindow>
 #include "ui_XtractCom.h"
 
+class QFileExplorer;
+class QCppCommentViewer;
+//class QTabWidget;
+
 class XtractCom : public QMainWindow
 {
 	Q_OBJECT
 
 public:
 	XtractCom(QWidget *parent = Q_NULLPTR);
+
+protected:
+	QFileExplorer * mQFileExplorer;
+	QCppCommentViewer * mQCppCommentViewer;
+	QTabWidget * mTabExplorer;
+	QTabWidget * mTabTask;
+
 
 private:
 	Ui::XtractComClass ui;
